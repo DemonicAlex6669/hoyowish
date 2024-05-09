8d1d206c4ab0fe3457f6e8e97d3f73d2acc0edbe
alex watson <alexwatsondemon@gmail.com>
2024-05-08 22:40:27 -0700

    v 1.0
    still need to add pages to last 5star

--- hoyowish.c
+++ hoyowish.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 	//taking into account whether the last 50/50 was won
-	if (strcmp(argv[1] == "won"))
+	if (strcmp(argv[1], "won") == 0)
 	{
 	int primo = atoi(argv[2]);
 	int wishes = primo / 160;
 
-	printf("you need %i to garentee\n", (90 - wishes) * 160 + (90 * 160) )
+	printf("you need %i to garentee\n", (90 - wishes) * 160 + (90 * 160) );
 	}
-	else if (strcmp(argv[1] == "lost"))
+	else if (strcmp(argv[1], "lost") == 0)
 	{
-	int primo = atoiargv[2];
+	int primo = atoi(argv[2]);
 	int wishes = primo / 160;
 
-	printf("you need %i to garentee\n", (90 - wishes) * 160)
+	printf("you need %i to garentee\n", (90 - wishes) * 160);
 	}
 }
