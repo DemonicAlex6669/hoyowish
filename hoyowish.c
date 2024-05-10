87320b3748cb53d29f076d167138b3673af79002
alex watson <alexwatsondemon@gmail.com>
2024-05-09 18:19:54 -0700

    2.0
    added page calculation

--- hoyowish.c
+++ hoyowish.c
@@ -8,14 +8,24 @@ int main (int argc, char *argv[])
 	{
 	int primo = atoi(argv[2]);
 	int wishes = primo / 160;
+	int pages = atoi(argv[3]);
+	int remains = atoi(argv[4]);
+	int pity =(90 - (pages * 5) + remains) * 160;
 
-	printf("you need %i to garentee\n", (90 - wishes) * 160 + (90 * 160) );
+	printf("you need %i to garentee\n", (90 - wishes) * 160 + pity );
 	}
 	else if (strcmp(argv[1], "lost") == 0)
 	{
 	int primo = atoi(argv[2]);
 	int wishes = primo / 160;
+	int pages = atoi(argv[3]);
+	int remains = atoi(argv[4]);
+	int pity =(90 - (pages * 5) + remains) * 160;
 
-	printf("you need %i to garentee\n", (90 - wishes) * 160);
+	printf("you need %i to garentee\n", (90 - wishes) * 160 + pity);
+	}
+	else if (strcmp(argv[1], "help"))
+	{
+	printf("enter ./hoyowish won #OfPrimos #OfPagesToLast5Star #OfRemaining\n");
 	}
 }
